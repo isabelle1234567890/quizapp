cb20c0ee267b3043edd242fb35fcf961ac6bae60
isabelle1234567890 americo <isabelleamericonunes@gmail.com>
2024-06-07 00:29:38 +0000

    adicionadas as funcoes do botao de proxima pergunta e de finalizar o quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -99,7 +99,7 @@ function montarPergunta() {
                         </div>
                     </label>
                 </form>
-                <button>Enviar</button>
+                <button>Responder</button>
         </section>
         
     
@@ -122,6 +122,17 @@ function guardarResposta(evento){
 
 
 function validarResposta(){
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+
+    if (pergunta === 10){
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
     if (resposta === quiz.questions[pergunta-1].answer){
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
         pontos = pontos + 1
@@ -129,6 +140,14 @@ function validarResposta(){
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
     }
+    
+    pergunta = pergunta + 1
+}
+
+function finalizar() {
+    localStorage.setItem("pontos", pontos)
+    window.location.href= "../resultado/resultado.html"
+
 }
 
 
@@ -137,16 +156,9 @@ async function iniciar(){
     alterarAssunto()
     await buscarPerguntas()
     montarPergunta()
+    adicionarEventoInputs()
 
-    
-    const inputsResposta = document.querySelectorAll(".alternativas input")
-    inputsResposta.forEach(input => {
-        input.addEventListener("click", guardarResposta)
-        
-        if (input.value === quiz.questions[pergunta-1].answer){
-            respostaCorretaId = input.id
-        }
-    })
+
 }
 
 iniciar()
